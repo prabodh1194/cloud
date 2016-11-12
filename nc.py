deb82ef4217b907a36f392895543a94de71b6a62
Prabodh Agarwal <prabodh1194@users.noreply.github.com>
2016-11-12 18:40:45 +0530

    implemented resource description

--- nc.py
+++ nc.py
@@ -1,4 +1,37 @@
 import libvirt
+from xml.dom import minidom
+
+def getVolume(conn, domID):
+    dom = conn.lookupByID(domID)
+    raw_xml = dom.XMLDesc(0)
+    xml = minidom.parseString(raw_xml)
+    diskTypes = xml.getElementsByTagName('disk')
+    for diskType in diskTypes:
+        nam = ""
+        typ = ""
+        fil = ""
+    
+        if diskType.getAttribute('device') != 'disk':
+            continue
+        diskNodes = diskType.childNodes
+        for diskNode in diskNodes:
+            if fil is not "":
+                print fil
+                break
+            attrs = diskNode.attributes
+            if attrs is not None:
+                for attr in attrs.keys():
+                    name = str(diskNode.attributes[attr].name)
+                    val = str(diskNode.attributes[attr].value)
+                    if diskNode.nodeName == 'driver':
+                        if name == 'name' and val == 'qemu':
+                            nam = val
+                        elif name == 'type' and val == 'qcow2':
+                            typ = val
+                    elif diskNode.nodeName == 'source':
+                        if name == 'file' and typ is not "" and nam is not "":
+                            fil = val
+                            break
 
 def createStoragePoolVolume(pool, name, size):
     stpVolXml = """
@@ -22,11 +55,11 @@ def createStoragePoolVolume(pool, name, size):
     stpVol = pool.createXML(stpVolXml, 0)
     return stpVol
 
-def createVM(conn, name, cpu, memory, size):
+def createVM(conn, pool, name, cpu, memory, size):
     xml = "<?xml version='1.0'?>\
             <domain type='kvm' id='8'>\
                 <name>"+name+"</name>\
-                <memory unit='KiB'>"+memory+"</memory>\
+                <memory unit='B'>"+memory+"</memory>\
                 <vcpu>"+cpu+"</vcpu>\
                 <os>\
                     <type>hvm</type>\
@@ -59,20 +92,44 @@ def createVM(conn, name, cpu, memory, size):
             </domain>\
             "
     
-    pool = conn.storagePoolLookupByName('images')
-    #createStoragePoolVolume(pool,name,size)
-    domain=conn.createXML(xml)
+    createStoragePoolVolume(pool,name,size)
+    domain=conn.defineXML(xml)
+    domain.create()
+    domain.setAutostart(1)
 
 def describeResources(conn,pool):
     resource = {}
-    resource.capacity = pool.info()[1]
-    resource.vcpu = conn.getMaxVcpus(None)
-    resource.memory = conn.getInfo()[1]
+    resource["capacity"] = pool.info()[1]
+    resource["vcpu"] = conn.getMaxVcpus(None)
+    resource["memory"] = (conn.getInfo()[1])*1024*1024
 
+    print resource
 
+    mem = 0
+    hdd = 0
+    cpu = 0
 
-memory = "2097152"
+    domains = conn.listAllDomains(0)
+    for dom in domains:
+
+        vol = pool.storageVolLookupByName(dom.name()+".qcow2")
+        vol = vol.info()
+
+        print dom.name(), dom.maxMemory()
+
+        mem += (dom.maxMemory()*1024)
+        cpu += dom.maxVcpus()
+        hdd += vol[1]
+
+    resource["capacity"] -= hdd
+    resource["vcpu"] -= cpu
+    resource["memory"] -= mem
+    return resource
+
+memory = "1073741824"
 cpu = "2"
 name = "centos71"
 conn=libvirt.open("qemu:///system")
-createVM(conn, name, cpu, memory, "10")
+pool = conn.storagePoolLookupByName('images')
+createVM(conn, pool, name, cpu, memory, "10")
+print describeResources(conn, pool)
